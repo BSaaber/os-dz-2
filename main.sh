534d53078eba26fa3748c8a777cc763b7f2b22c8
Max <66118806+BSaaber@users.noreply.github.com>
2023-02-10 22:40:41 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 a=5;
-while ((a > 0)); do
-echo "a value:";
+while ((a > 0)); do # cycle. while a is bigger than 0
+echo "a value:"; # here and below "echo" - print in terminal
 echo $a;
-if ((a==5));
+if ((a==5)); # if condition
 then
-echo "a is 5";
+echo "a is 5"; # if body
 else
-echo "a is not 5";
-fi;
-((a--));
-done
+echo "a is not 5"; # else body
+fi; # end of if
+((a--)); # decrement
+done # end of while
 echo "the end";
